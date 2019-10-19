6bf8100723bf6347f0ea94ff7aeb1e77aa4e74bb
billwang80 <billwang80@users.noreply.github.com>
2019-10-19 00:07:33 -0400

    Minor edits

--- src/world_wind.js
+++ src/world_wind.js
@@ -1,9 +1,20 @@
 var wwd = new WorldWind.WorldWindow("canvasOne");
-
 // wwd layers
+var layers = [
+    {layer: new WorldWind.BMNGOneImageLayer(), enabled: true},
+    {layer: new WorldWind.BMNGLandsatLayer(), enabled: true},
+    {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+    {layer: new WorldWind.AtmosphereLayer(), enabled: true},
+    {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+    {layer: new WorldWind.BMNGOneImageLayer(), enabled: true},
+
+]
+
+
 wwd.addLayer(new WorldWind.BMNGOneImageLayer());
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 wwd.addLayer(new WorldWind.BingAerialWithLabelsLayer(null));
+wwd.addLayer(new WorldWind.AtmosphereLayer());
 //wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
 //wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
@@ -22,5 +33,22 @@ var createLayer = function (xmlDom) {
 var logError = function(jqXhr, text, exception) {
     console.log("Failure retriving capabilities: " + text + " exception: " + exception);
 };
+
+var enabledheat = false;
+$(document).ready(function(){
+    $("#test_button").click(function() {
+        console.log("working");
+        enabledheat = !enabledheat;
+        var heatLayer = "MOD14A1_M_FIRE";
+        if (enabledheat) {
+            console.log("XD");
+            wwd.addLayer(heatLayer);
+
+        } else {
+            console.log('not xd');
+            wwd.remove(heatLayer);
+        }
+    });
+});
 
 $.get(serviceAddress).done(createLayer).fail(logError);
